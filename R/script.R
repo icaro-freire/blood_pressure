f4a8083ba049c87eb64795e0ebf382fd33811422
icaro-freire <icarofreire7@gmail.com>
2023-10-01 17:04:13 -0300

    <mod> Modificação na estrurura do dataset (relocate)

--- R/script.R
+++ R/script.R
@@ -1,4 +1,7 @@
 # carregando dados
 dados <- readr::read_csv("../data/blood-pressure_icaro.csv")
 
+novo_dados <- dados |> 
+  dplyr::relocate(time, .after = date)
 
+readr::write_csv(novo_dados, "../data/blood-pressure_icaro-novo.csv")
